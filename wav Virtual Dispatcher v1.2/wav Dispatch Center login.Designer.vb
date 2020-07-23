fb60bd2831c57117e80640f437243be0108e105d
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2020-07-23 17:06:51 +0100

    j64yr

--- wav Virtual Dispatcher v1.2/wav Dispatch Center login.Designer.vb
+++ wav Virtual Dispatcher v1.2/wav Dispatch Center login.Designer.vb
@@ -119,7 +119,7 @@ Partial Class Form2
         Me.Label1.AutoSize = True
         Me.Label1.BackColor = System.Drawing.Color.White
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(13, 9)
+        Me.Label1.Location = New System.Drawing.Point(15, -5)
         Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(790, 54)
