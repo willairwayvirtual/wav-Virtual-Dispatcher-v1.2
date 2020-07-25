4d0556bde7291a7050c90359aa89c70f74127c72
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2020-07-25 16:04:12 +0100

    bsbfdwasvy

--- wav Virtual Dispatcher v1.2/.Designer.vb
+++ wav Virtual Dispatcher v1.2/.Designer.vb
@@ -43,10 +43,10 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(450, 367)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(496, 367)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(56, 32)
+        Me.Label1.Size = New System.Drawing.Size(77, 46)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "0%"
         '
