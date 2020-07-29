a59a62bf277e7e6855b437e3ec6205b546c8ee30
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2020-07-29 11:27:22 +0100

    nfdv;[#'yerdfgpl[

--- wav Virtual Dispatcher v1.2/Form3.Designer.vb
+++ wav Virtual Dispatcher v1.2/Form3.Designer.vb
@@ -32,12 +32,19 @@ Partial Class Form3mll
         Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.PrintToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.flightinfo = New System.Windows.Forms.Button()
+        Me.AircraftType = New System.Windows.Forms.Button()
+        Me.PAYLOAD = New System.Windows.Forms.Button()
+        Me.Route = New System.Windows.Forms.Button()
+        Me.alternates = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.save1 = New System.Windows.Forms.Button()
         Me.wavMenu.SuspendLayout()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(1412, 1)
+        Me.Button1.Location = New System.Drawing.Point(1567, 1)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(156, 27)
         Me.Button1.TabIndex = 1
@@ -46,7 +53,7 @@ Partial Class Form3mll
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(1217, 1)
+        Me.Button2.Location = New System.Drawing.Point(1372, 0)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(189, 27)
         Me.Button2.TabIndex = 2
@@ -59,7 +66,7 @@ Partial Class Form3mll
         Me.wavMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
         Me.wavMenu.Location = New System.Drawing.Point(0, 0)
         Me.wavMenu.Name = "wavMenu"
-        Me.wavMenu.Size = New System.Drawing.Size(1660, 28)
+        Me.wavMenu.Size = New System.Drawing.Size(1723, 28)
         Me.wavMenu.TabIndex = 3
         Me.wavMenu.Text = "MenuStrip1"
         '
@@ -74,47 +81,146 @@ Partial Class Form3mll
         '
         Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
         Me.NewToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
-        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(179, 26)
         Me.NewToolStripMenuItem.Text = "new"
         '
         'OpenToolStripMenuItem
         '
         Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
         Me.OpenToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
-        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(179, 26)
         Me.OpenToolStripMenuItem.Text = "open"
         '
         'SaveToolStripMenuItem
         '
         Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
         Me.SaveToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
-        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(179, 26)
         Me.SaveToolStripMenuItem.Text = "save"
         '
         'PrintToolStripMenuItem
         '
         Me.PrintToolStripMenuItem.Name = "PrintToolStripMenuItem"
         Me.PrintToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.P), System.Windows.Forms.Keys)
-        Me.PrintToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.PrintToolStripMenuItem.Size = New System.Drawing.Size(179, 26)
         Me.PrintToolStripMenuItem.Text = "print"
         '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
         Me.ExitToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.E), System.Windows.Forms.Keys)
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(179, 26)
         Me.ExitToolStripMenuItem.Text = "exit"
+        '
+        'flightinfo
+        '
+        Me.flightinfo.BackColor = System.Drawing.Color.DimGray
+        Me.flightinfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.flightinfo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.flightinfo.ForeColor = System.Drawing.Color.DarkRed
+        Me.flightinfo.Location = New System.Drawing.Point(0, 35)
+        Me.flightinfo.Margin = New System.Windows.Forms.Padding(4)
+        Me.flightinfo.Name = "flightinfo"
+        Me.flightinfo.Size = New System.Drawing.Size(1723, 44)
+        Me.flightinfo.TabIndex = 36
+        Me.flightinfo.Text = "Flight Info"
+        Me.flightinfo.UseVisualStyleBackColor = False
+        '
+        'AircraftType
+        '
+        Me.AircraftType.BackColor = System.Drawing.Color.DimGray
+        Me.AircraftType.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.AircraftType.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AircraftType.ForeColor = System.Drawing.Color.DarkRed
+        Me.AircraftType.Location = New System.Drawing.Point(0, 181)
+        Me.AircraftType.Margin = New System.Windows.Forms.Padding(4)
+        Me.AircraftType.Name = "AircraftType"
+        Me.AircraftType.Size = New System.Drawing.Size(1723, 37)
+        Me.AircraftType.TabIndex = 37
+        Me.AircraftType.Text = "AircraftType"
+        Me.AircraftType.UseVisualStyleBackColor = False
+        '
+        'PAYLOAD
+        '
+        Me.PAYLOAD.BackColor = System.Drawing.Color.DimGray
+        Me.PAYLOAD.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.PAYLOAD.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.PAYLOAD.ForeColor = System.Drawing.Color.DarkRed
+        Me.PAYLOAD.Location = New System.Drawing.Point(0, 303)
+        Me.PAYLOAD.Margin = New System.Windows.Forms.Padding(4)
+        Me.PAYLOAD.Name = "PAYLOAD"
+        Me.PAYLOAD.Size = New System.Drawing.Size(1736, 38)
+        Me.PAYLOAD.TabIndex = 38
+        Me.PAYLOAD.Text = "PAYLOAD"
+        Me.PAYLOAD.UseVisualStyleBackColor = False
+        '
+        'Route
+        '
+        Me.Route.BackColor = System.Drawing.Color.DimGray
+        Me.Route.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Route.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Route.ForeColor = System.Drawing.Color.DarkRed
+        Me.Route.Location = New System.Drawing.Point(0, 413)
+        Me.Route.Margin = New System.Windows.Forms.Padding(4)
+        Me.Route.Name = "Route"
+        Me.Route.Size = New System.Drawing.Size(1736, 37)
+        Me.Route.TabIndex = 39
+        Me.Route.Text = "Route"
+        Me.Route.UseVisualStyleBackColor = False
+        '
+        'alternates
+        '
+        Me.alternates.BackColor = System.Drawing.Color.DimGray
+        Me.alternates.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.alternates.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.alternates.ForeColor = System.Drawing.Color.DarkRed
+        Me.alternates.Location = New System.Drawing.Point(-16, 547)
+        Me.alternates.Margin = New System.Windows.Forms.Padding(4)
+        Me.alternates.Name = "alternates"
+        Me.alternates.Size = New System.Drawing.Size(1739, 42)
+        Me.alternates.TabIndex = 40
+        Me.alternates.Text = "alternates"
+        Me.alternates.UseVisualStyleBackColor = False
+        '
+        'Button12
+        '
+        Me.Button12.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button12.Image = CType(resources.GetObject("Button12.Image"), System.Drawing.Image)
+        Me.Button12.Location = New System.Drawing.Point(0, 683)
+        Me.Button12.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(1736, 65)
+        Me.Button12.TabIndex = 45
+        Me.Button12.Text = "complete flight"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'save1
+        '
+        Me.save1.Location = New System.Drawing.Point(0, 768)
+        Me.save1.Margin = New System.Windows.Forms.Padding(4)
+        Me.save1.Name = "save1"
+        Me.save1.Size = New System.Drawing.Size(1736, 48)
+        Me.save1.TabIndex = 47
+        Me.save1.Text = "save"
+        Me.save1.UseVisualStyleBackColor = True
         '
         'Form3mll
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ButtonShadow
-        Me.ClientSize = New System.Drawing.Size(1660, 725)
+        Me.ClientSize = New System.Drawing.Size(1723, 873)
+        Me.Controls.Add(Me.save1)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.alternates)
+        Me.Controls.Add(Me.Route)
+        Me.Controls.Add(Me.PAYLOAD)
+        Me.Controls.Add(Me.AircraftType)
+        Me.Controls.Add(Me.flightinfo)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.wavMenu)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MainMenuStrip = Me.wavMenu
         Me.Name = "Form3mll"
@@ -135,4 +241,11 @@ Partial Class Form3mll
     Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents PrintToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents flightinfo As Button
+    Friend WithEvents AircraftType As Button
+    Friend WithEvents PAYLOAD As Button
+    Friend WithEvents Route As Button
+    Friend WithEvents alternates As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents save1 As Button
 End Class
